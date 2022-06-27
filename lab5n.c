c97452d7b0688a5c09e4ea40284d172a575f6b20
vikasvenki <93909839+vikasvenki@users.noreply.github.com>
2022-06-27 22:01:07 +0530

    Update lab5n.c

--- lab5n.c
+++ lab5n.c
@@ -70,7 +70,7 @@ int main(int argc,char*argv[])
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
 	glutInitWindowSize(500,500);
-	glutCreateWindow("3D GASKET 4MT19CS177");
+	glutCreateWindow("3D GASKET 4MT19CS176");
 	glutReshapeFunc(myReshape);
 	glutDisplayFunc(display);
 	glEnable(GL_DEPTH_TEST);
